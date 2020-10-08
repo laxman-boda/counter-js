e6ce58cf837a1488146a7e6693c36829a488fd6a
laxman-boda <laxman.boda96@gmail.com>
2020-10-08 20:19:05 +0530

    implement counter

---
+++ code.js
@@ -0,0 +1,11 @@
+function myFun() {
+    var x ,text;
+    x=document.getElementById("numb").value;
+    if (x <1 || x<15) {
+        text="input not valid";
+        
+    }else {
+        text="input valid";
+    }
+    document.getElementById("demo").text
+}
